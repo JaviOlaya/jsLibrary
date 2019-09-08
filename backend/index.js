34c9434c6d15e5f1e9f602ac3ed262f862c0b523
javiOlaya <jjosaez@gmail.com>
2019-09-09 01:08:24 +0200

    configuration multer

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const multer = require('multer');
+const path = require ('path');
 //initializations
 const app = express();
 
@@ -8,7 +9,15 @@ const app = express();
 app.set('port',3000);
 
 //Definition of Middleware
-app.use(morgan());
+app.use(morgan('dev'));
+const storage = multer.diskStorage({
+        destination: path.join(__dirname, 'public/uploads'),
+        filename(req, file, cb){
+                cb(null, new Date().get.Time()+ path.extname(file.originalname));
+
+        }
+})
+app.use(multer({storage}).single('image'));
 
 // Start the server on port 3000
 app.listen(app.get('port'), () => {
