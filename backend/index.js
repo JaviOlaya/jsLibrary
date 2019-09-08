a486f435c64c2718dc56151cae764571e73e2795
javiOlaya <jjosaez@gmail.com>
2019-09-09 00:53:34 +0200

    require morgan and multer

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
-
+const morgan = require('morgan');
+const multer = require('multer');
 //initializations
 const app = express();
 
 //definitioon of variable port
 app.set('port',3000);
+
+//Definition of Middleware
+app.use(morgan());
 
 // Start the server on port 3000
 app.listen(app.get('port'), () => {
